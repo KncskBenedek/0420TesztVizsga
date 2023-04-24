fb82adcaf9a904d475b5c0a081fc7046faed9956
KncskBenedek <koncsikbenedek@gmail.com>
2023-04-24 16:14:41 +0200

    alap frontend

--- IngatlanFrontend/src/app/app.component.ts
+++ IngatlanFrontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingatlan, Kategoria } from './shared/interfaces';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'IngatlanFrontend';
+  ingatlanok: Ingatlan[] =[{
+    kategoria:"Ház",
+    katId: 1,
+    leiras: "a",
+    hirdDatum: "2023-02-03",
+    tehermentes: true,
+    kepUrl: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Google_Chrome_icon_%28February_2022%29.svg"
+  },{
+    kategoria:"Ház",
+    katId: 1,
+    leiras: "a",
+    hirdDatum: "2023-02-03",
+    tehermentes: true,
+    kepUrl: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Google_Chrome_icon_%28February_2022%29.svg"
+  },{
+    kategoria:"Ház",
+    katId: 1,
+    leiras: "a",
+    hirdDatum: "2023-02-03",
+    tehermentes: true,
+    kepUrl: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Google_Chrome_icon_%28February_2022%29.svg"
+  },{
+    kategoria:"Ház",
+    katId: 1,
+    leiras: "a",
+    hirdDatum: "2023-02-03",
+    tehermentes: true,
+    kepUrl: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Google_Chrome_icon_%28February_2022%29.svg"
+  }]
+
+
+  kategoriak: Kategoria[] = [
+    {
+      id:1,
+      kategoria: "Ház"
+    },{
+      id:2,
+      kategoria: "Garázs"
+    },{
+      id:3,
+      kategoria: "farm"
+    },{
+      id:4,
+      kategoria: "cica"
+    }
+  ];
 }
---
+++ IngatlanFrontend/src/app/shared/interfaces.ts
@@ -0,0 +1,14 @@
+export  interface Ingatlan{
+ kategoria: string;
+ katId: number;
+ leiras: string;
+ hirdDatum: string;
+ tehermentes: boolean;
+ kepUrl: string;
+}
+
+
+export interface Kategoria {
+    kategoria: string;
+    id: number;
+}
